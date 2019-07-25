da76e45417bfcf1ac21fb7d0b3ed243e71c937c7
Kumo <a6934000@gmail.com>
2019-07-25 17:19:41 +0800

    prod.env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,9 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  APIPATH: '"https://vue-course-api.hexschool.io"',
-  CUSTOMPATH: '"candle"',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/candle/'
+    : '/'
+  // APIPATH: '"https://vue-course-api.hexschool.io"',
+  // CUSTOMPATH: '"candle"',
 }
