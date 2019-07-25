a88b8c35cace293b8afc4ab13556a157edf05a69
Kumo <a6934000@gmail.com>
2019-07-25 17:45:09 +0800

    dist3

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/KumoSHIH/candle',
+    assetsPublicPath: '/KumoSHIH/candle/',
 
     /**
      * Source Maps
